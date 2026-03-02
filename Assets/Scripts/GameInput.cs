aa1b6ea2568b48cee24b7b17308f87acefade94d
pilmeha <garmayev.ayur@inbox.ru>
2026-03-02 15:21:44 +0500

    refactoring inputSystem

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameInput : MonoBehaviour
+{
+
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+
+        inputVector = inputVector.normalized;
+        Debug.Log(inputVector);
+
+        return inputVector;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,30 +4,13 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Keyboard.current.wKey.isPressed)
-        {
-            inputVector.y = +1;
-        }
-        if (Keyboard.current.sKey.isPressed)
-        {
-            inputVector.y = -1;
-        }
-        if (Keyboard.current.aKey.isPressed)
-        {
-            inputVector.x = -1;
-        }
-        if (Keyboard.current.dKey.isPressed)
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();  
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
@@ -36,8 +19,6 @@ public class Player : MonoBehaviour
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-
-        Debug.Log(Time.deltaTime);
     }
 
     public bool IsWalking()
