19cb75c72a9a47c523d8ea19369b0c79ea5dec68
pilmeha <garmayev.ayur@inbox.ru>
2026-03-04 00:43:45 +0500

    selected items

---
+++ Assets/Scripts/SelectedCounterVisual.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+public class SelectedCounterVisual : MonoBehaviour
+{
+
+    [SerializeField] private ClearCounter clearCounter;
+    [SerializeField] private GameObject visualGameObject;
+    
+    private void Start()
+    {
+        Player.Instance.OnSelectedCounterChanged += Player_onSelectedCounterChanged;
+    }
+
+    private void Player_onSelectedCounterChanged(object sender, Player.OnSelectedCounterChangedEventArgs e)
+    {
+        if (e.selectedCounter == clearCounter)
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    private void Show()
+    {
+        visualGameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        visualGameObject.SetActive(false);
+    }
+}
