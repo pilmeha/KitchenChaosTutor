802aae640118a717c3985b7d9f25828d6666414b
pilmeha <garmayev.ayur@inbox.ru>
2026-03-30 17:19:10 +0500

    добавил другие типы контейнеров (салат, котлета, булка)

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -6,6 +6,23 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     { 
+        if (!HasKitchenObject())
+        {
+            if (player.HasKitchenObject()) {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            } else
+            {
 
+            }
+        } else
+        {
+            if (player.HasKitchenObject())
+            {
+
+            } else
+            {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
