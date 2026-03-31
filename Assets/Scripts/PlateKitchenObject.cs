4d6e16efacfe90d81442910fb17efc525a61bc9d
pilmeha <garmayev.ayur@inbox.ru>
2026-03-31 23:12:03 +0500

    add visual for ingredients on the plate

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,17 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
 
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
 
@@ -27,7 +35,14 @@ public class PlateKitchenObject : KitchenObject
         else
         {   
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+            {
+                kitchenObjectSO = kitchenObjectSO
+            });
             return true;
         }
     }
+
+
 }
