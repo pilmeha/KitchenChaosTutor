06d70a155140fe03ab55124680a1ca1b93c5f199
pilmeha <garmayev.ayur@inbox.ru>
2026-04-01 00:23:57 +0500

    add ui ingredients on the plate

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -44,5 +44,10 @@ public class PlateKitchenObject : KitchenObject
         }
     }
 
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
+    }
+
 
 }
